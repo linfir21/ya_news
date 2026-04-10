219eb37747cbfee9e31484af166339516da3e1b7
Alex Melnikov <coyotete21@gmail.com>
2026-04-10 19:48:40 +0300

    moded

---
+++ pytest_trial/test_super.py
@@ -0,0 +1,23 @@
+# test_super.py
+def test_yandex():
+    assert True
+
+
+def test_google():
+    assert False
+
+
+def test_google_or_yandex():
+    assert 'google' == 'yandex'
+
+
+def test_java():
+    assert "2" + "2" + "2" == "222"
+
+
+def test_javascript():
+    assert "2" + "2" - "2" == 20
+
+
+def test_python():
+    assert 2 + 2 - 2 == 2
